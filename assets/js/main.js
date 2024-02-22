ff035eb57ff4a6781625c25f4ba7ff8a00420033
PRAVEEN200307 <crouselearn@gmail.com>
2024-02-22 09:23:58 -0500

    fix: added full features

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,27 @@
-// Write your javascript code here
+// https://www.tamildailycalendar.com/2024/25022024.jpg
+
+const dayImage = document.querySelector('#dayImage');
+
+
+const inputEl = document.querySelector('#userData');
+
+inputEl.addEventListener('change', (e) => {
+  const value = e.target.value;
+  processData(value)
+});
+
+
+function processData(customDate) {
+
+  let date =customDate?new Date(customDate):new Date();
+  const result = date.toLocaleDateString("en-IN", {
+    year: "numeric",
+    month: "2-digit",
+    day: "numeric",
+  })
+  
+  const datedetail = result.replaceAll('/', '');
+  dayImage.src = `https://www.tamildailycalendar.com/${date.getFullYear()}/${datedetail}.jpg`;
+
+}
+processData()
